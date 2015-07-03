34e5d3bde718d29d9315e3693843254aaa4e520f
Dan Comand <dcomand@gmail.com>
2015-07-02 22:32:44 -0400

    Added user_id to meals table and added log in logic.

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -17,6 +17,7 @@ class MealsController < ApplicationController
 
   def create
     @meal = Meal.new(meal_params)
+    @meal.user_id = current_user.id
 
     if @meal.save
       redirect_to meals_url
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150703014828) do
+ActiveRecord::Schema.define(version: 20150703022235) do
 
   create_table "meals", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20150703014828) do
     t.integer  "servingQuantity"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
