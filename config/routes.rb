25ccb5e55a62acd205864e483c484d63883be25f
Dan Comand <dcomand@gmail.com>
2015-07-03 00:18:06 -0400

    Adding routes and views for users/show page.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
 
 
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
 
   resources :meals do
